a781fd229ef846072f1cbd68f394f6e29de2e045
oozaw <79121990+oozaw@users.noreply.github.com>
2023-06-08 08:01:04 +0700

    (^) index pengajuan wilayah on admin view

--- src/views/admin/menu-utama/pengajuan-wilayah/Index.vue
+++ src/views/admin/menu-utama/pengajuan-wilayah/Index.vue
@@ -189,7 +189,7 @@
                         tabindex="-1"
                         aria-hidden="true"
                       >
-                        <div class="modal-dialog mt-lg-10">
+                        <div class="modal-dialog modal-lg mt-lg-10">
                           <div class="modal-content">
                             <div class="modal-header">
                               <h5 id="ModalLabel" class="modal-title">
