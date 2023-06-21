d9bfc1fb14aa923a760998d4a4792a370a3e02c7
oozaw <79121990+oozaw@users.noreply.github.com>
2023-06-21 10:55:06 +0700

    (+) detail lpk view on dosen

--- src/views/kegiatan/lpk/Index.vue
+++ src/views/kegiatan/lpk/Index.vue
@@ -246,14 +246,175 @@
                       </td>
                       <td class="text-sm">
                         <a
-                          href="javascript:;"
-                          class="me-3"
-                          data-bs-toggle="tooltip"
-                          data-bs-original-title="Detail LPK"
-                          title="Detail LPK"
+                          type="button"
+                          class="mb-0 me-3 text-primary"
+                          data-bs-toggle="modal"
+                          :data-bs-target="'#detail_' + lpk.id_laporan"
+                          title="Detail Laporan"
                         >
                           <i class="fas fa-eye text-info"></i>
                         </a>
+                        <div
+                          :id="'detail_' + lpk.id_laporan"
+                          class="modal fade"
+                          tabindex="-1"
+                          aria-hidden="true"
+                        >
+                          <div class="modal-dialog modal-lg mt-lg-5">
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <h5 id="ModalLabel" class="modal-title">
+                                  Detail Laporan
+                                  {{ lpk.mahasiswa.nama }}
+                                </h5>
+                                <button
+                                  type="button"
+                                  class="btn-close text-dark mb-0"
+                                  data-bs-dismiss="modal"
+                                  aria-label="Close"
+                                >
+                                  <font-awesome-icon icon="fa-solid fa-xmark" />
+                                </button>
+                              </div>
+                              <div class="modal-body p-4">
+                                <ul class="list-group">
+                                  <li
+                                    class="pt-0 text-sm border-0 list-group-item ps-0"
+                                  >
+                                    <strong class="text-dark"
+                                      >Nama Lengkap:</strong
+                                    >
+                                    &nbsp;
+                                    {{ lpk.mahasiswa.nama }}
+                                  </li>
+                                  <li
+                                    class="text-sm border-0 list-group-item ps-0"
+                                  >
+                                    <strong class="text-dark">NIM:</strong>
+                                    &nbsp;
+                                    {{ lpk.mahasiswa.nim }}
+                                  </li>
+                                  <li
+                                    class="text-sm border-0 list-group-item ps-0"
+                                  >
+                                    <strong class="text-dark">Fakultas:</strong>
+                                    &nbsp;
+                                    {{ lpk.mahasiswa.prodi.fakultas.nama }}
+                                  </li>
+                                  <li
+                                    class="text-sm border-0 list-group-item ps-0"
+                                  >
+                                    <strong class="text-dark">Prodi:</strong>
+                                    &nbsp;
+                                    {{ lpk.mahasiswa.prodi.nama }}
+                                  </li>
+                                  <li
+                                    class="text-sm border-0 list-group-item ps-0"
+                                  >
+                                    <strong class="text-dark">Judul:</strong>
+                                    &nbsp;
+                                    <div
+                                      class="text-wrap"
+                                      v-html="getOutOfTagP(lpk.program)"
+                                    ></div>
+                                  </li>
+                                  <li
+                                    class="text-sm border-0 list-group-item ps-0"
+                                  >
+                                    <strong class="text-dark">Kategori:</strong>
+                                    &nbsp;
+                                    {{
+                                      lpk.kategori == 1
+                                        ? "Monodisiplin"
+                                        : "Multidisiplin"
+                                    }}
+                                  </li>
+                                  <li
+                                    class="text-sm border-0 list-group-item ps-0"
+                                  >
+                                    <strong class="text-dark">Potensi:</strong>
+                                    &nbsp;
+                                    <div
+                                      class="text-wrap"
+                                      v-html="getOutOfTagP(lpk.potensi)"
+                                    ></div>
+                                  </li>
+                                  <li
+                                    class="text-sm border-0 list-group-item ps-0"
+                                  >
+                                    <strong class="text-dark">Sasaran:</strong>
+                                    &nbsp;
+                                    <div
+                                      class="text-wrap"
+                                      v-html="getOutOfTagP(lpk.sasaran)"
+                                    ></div>
+                                  </li>
+                                  <li
+                                    class="text-sm border-0 list-group-item ps-0"
+                                  >
+                                    <strong class="text-dark">Metode:</strong>
+                                    &nbsp;
+                                    <div
+                                      class="text-wrap"
+                                      v-html="getOutOfTagP(lpk.metode)"
+                                    ></div>
+                                  </li>
+                                  <li class="border-0 list-group-item ps-0">
+                                    <strong class="text-sm text-dark"
+                                      >Keluaran:</strong
+                                    >
+                                    &nbsp;
+                                    <div
+                                      class="text-wrap"
+                                      v-html="getOutOfTagP(lpk.luaran)"
+                                    ></div>
+                                  </li>
+                                  <li class="border-0 list-group-item ps-0">
+                                    <strong class="text-sm text-dark"
+                                      >Pelaksanaan:</strong
+                                    >
+                                    &nbsp;
+                                    <div
+                                      class="text-wrap"
+                                      v-html="getOutOfTagP(lpk.pelaksanaan)"
+                                    ></div>
+                                  </li>
+                                  <li class="border-0 list-group-item ps-0">
+                                    <strong class="text-sm text-dark"
+                                      >Capaian:</strong
+                                    >
+                                    &nbsp;
+                                    <div
+                                      class="text-wrap"
+                                      v-html="getOutOfTagP(lpk.capaian)"
+                                    ></div>
+                                  </li>
+                                  <li class="border-0 list-group-item ps-0">
+                                    <strong class="text-sm text-dark"
+                                      >Hambatan:</strong
+                                    >
+                                    &nbsp;
+                                    <div
+                                      class="text-wrap"
+                                      v-html="getOutOfTagP(lpk.hambatan)"
+                                    ></div>
+                                  </li>
+                                  <li class="border-0 list-group-item ps-0">
+                                    <strong class="text-sm text-dark"
+                                      >Komentar:</strong
+                                    >
+                                    &nbsp;
+                                    <div
+                                      class="text-wrap"
+                                      v-html="getOutOfTagP(lpk.komentar)"
+                                    ></div>
+                                  </li>
+                                </ul>
+                              </div>
+                              <div class="modal-footer"></div>
+                            </div>
+                          </div>
+                        </div>
                         <a
                           href="javascript:;"
                           data-bs-toggle="tooltip"
