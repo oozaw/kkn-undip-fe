54a0f9af51b3ab4aa66a7628a46946d2616ea197
oozaw <79121990+oozaw@users.noreply.github.com>
2023-07-26 16:36:11 +0700

    fix dosen

--- src/service/dosen.js
+++ src/service/dosen.js
@@ -4,21 +4,20 @@ const api = `/`;
 
 const listDosen = () => baseApi.get(`${api}dosen`);
 
-const getDosen = (id_dosen) => baseApi.get(`${api}dosen/${id_dosen}`);
+const getDosen = (id_dosen) => baseApi.get(`${api}dosen/detail/${id_dosen}`);
 
-const addDosen = (body) => baseApi.post(`${api}admin/dosen/single`, body);
+const addDosen = (body) => baseApi.post(`${api}dosen`, body);
 
 const importDosen = (body) =>
-  baseApi.post(`${api}admin/dosen`, body, {
+  baseApi.post(`${api}dosen/import`, body, {
     headers: {
       "Content-Type": "multipart/form-data",
     },
   });
 
 const editDosen = (id_dosen, body) =>
-  baseApi.put(`${api}admin/dosen/${id_dosen}`, body);
+  baseApi.put(`${api}dosen/${id_dosen}`, body);
 
-const deleteDosen = (id_dosen) =>
-  baseApi.delete(`${api}admin/dosen/${id_dosen}`);
+const deleteDosen = (id_dosen) => baseApi.delete(`${api}dosen/${id_dosen}`);
 
 export { listDosen, getDosen, addDosen, importDosen, editDosen, deleteDosen };
