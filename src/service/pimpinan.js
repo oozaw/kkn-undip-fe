d0086fc119329075790c29b100f0b50fcb08e213
oozaw <79121990+oozaw@users.noreply.github.com>
2023-07-26 17:04:37 +0700

    fix pimpinan

--- src/service/pimpinan.js
+++ src/service/pimpinan.js
@@ -5,22 +5,22 @@ const api = `/`;
 const listPimpinan = () => baseApi.get(`${api}pimpinan`);
 
 const getPimpinan = (id_pimpinan) =>
-  baseApi.get(`${api}pimpinan/${id_pimpinan}`);
+  baseApi.get(`${api}pimpinan/detail/${id_pimpinan}`);
 
-const addPimpinan = (body) => baseApi.post(`${api}admin/pimpinan/single`, body);
+const addPimpinan = (body) => baseApi.post(`${api}pimpinan`, body);
 
 const importPimpinan = (body) =>
-  baseApi.post(`${api}admin/pimpinan`, body, {
+  baseApi.post(`${api}pimpinan/import`, body, {
     headers: {
       "Content-Type": "multipart/form-data",
     },
   });
 
 const editPimpinan = (id_pimpinan, body) =>
-  baseApi.put(`${api}admin/pimpinan/${id_pimpinan}`, body);
+  baseApi.put(`${api}pimpinan/${id_pimpinan}`, body);
 
 const deletePimpinan = (id_pimpinan) =>
-  baseApi.delete(`${api}admin/pimpinan/${id_pimpinan}`);
+  baseApi.delete(`${api}pimpinan/${id_pimpinan}`);
 
 export {
   listPimpinan,
