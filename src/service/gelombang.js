ef746e7123e4cf22ccdcd686c2fc3d1a7769c27a
oozaw <79121990+oozaw@users.noreply.github.com>
2023-07-26 10:23:15 +0700

    fix gelombang

--- src/service/gelombang.js
+++ src/service/gelombang.js
@@ -2,11 +2,10 @@ import { baseApi } from "@/utils/axios";
 
 const api = `/`;
 
-const listAllGelombang = (id_tema) =>
-  baseApi.get(`${api}admin/gelombang/${id_tema}`);
+const listAllGelombang = (id_tema) => baseApi.get(`${api}gelombang/${id_tema}`);
 
 const listGelombang = (id_tema, id_halaman) =>
-  baseApi.get(`${api}gelombang/${id_tema}/${id_halaman}`);
+  baseApi.get(`${api}gelombang/halaman/${id_tema}/${id_halaman}`);
 
 const listGelombangDosen = (id_tema, id_halaman, id_dosen) =>
   baseApi.get(`${api}gelombang/dosen/${id_tema}/${id_halaman}/${id_dosen}`);
@@ -17,18 +16,18 @@ const listGelombangMahasiswa = (id_tema, id_halaman, id_mahasiswa) =>
   );
 
 const getGelombang = (id_gelombang) =>
-  baseApi.get(`${api}admin/gelombang/detail/${id_gelombang}`);
+  baseApi.get(`${api}gelombang/detail/${id_gelombang}`);
 
-const addGelombang = (body) => baseApi.post(`${api}admin/gelombang`, body);
+const addGelombang = (body) => baseApi.post(`${api}gelombang`, body);
 
 const editGelombang = (id_gelombang, body) =>
-  baseApi.put(`${api}admin/gelombang/edit/${id_gelombang}`, body);
+  baseApi.put(`${api}gelombang/${id_gelombang}`, body);
 
 const switchGelombang = (id_gelombang) =>
-  baseApi.patch(`${api}admin/gelombang/${id_gelombang}`);
+  baseApi.patch(`${api}gelombang/${id_gelombang}`);
 
 const deleteGelombang = (id_gelombang) =>
-  baseApi.delete(`${api}admin/gelombang/${id_gelombang}`);
+  baseApi.delete(`${api}gelombang/${id_gelombang}`);
 
 export {
   listAllGelombang,
