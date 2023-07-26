b985fed75250d18d278e33dfa1488c0940a9f6e6
oozaw <79121990+oozaw@users.noreply.github.com>
2023-07-27 06:55:08 +0700

    fix reportase

--- src/service/reportase.js
+++ src/service/reportase.js
@@ -2,20 +2,20 @@ import { baseApi } from "@/utils/axios";
 
 const api = `/`;
 
-const listReportase = () => baseApi.get(`${api}mahasiswa/reportase`);
+const listReportase = () => baseApi.get(`${api}reportase/mahasiswa`);
 
 const listReportaseKecamatan = (id_kecamatan) =>
   baseApi.get(`${api}reportase/kecamatan/${id_kecamatan}`);
 
 const getReportase = (id_reportase) =>
-  baseApi.get(`${api}reportase/${id_reportase}`);
+  baseApi.get(`${api}reportase/detail/${id_reportase}`);
 
-const addReportase = (body) => baseApi.post(`${api}mahasiswa/reportase`, body);
+const addReportase = (body) => baseApi.post(`${api}reportase`, body);
 
 const editReportase = (id_reportase, body) =>
-  baseApi.put(`${api}mahasiswa/reportase/${id_reportase}`, body);
+  baseApi.put(`${api}reportase/${id_reportase}`, body);
 
-const evalReportase = (body) => baseApi.put(`${api}dosen/reportase`, body);
+const evalReportase = (body) => baseApi.put(`${api}reportase/evaluate`, body);
 
 const deleteReportase = (id_reportase) =>
   baseApi.delete(`${api}reportase/${id_reportase}`);
