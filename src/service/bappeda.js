738a95498f0dd681c517af636adf86a3a93e89f5
oozaw <79121990+oozaw@users.noreply.github.com>
2023-07-26 16:53:04 +0700

    fix bappeda

--- src/service/bappeda.js
+++ src/service/bappeda.js
@@ -4,24 +4,25 @@ const api = `/`;
 
 const listBappeda = () => baseApi.get(`${api}bappeda`);
 
-const getBappeda = (id_bappeda) => baseApi.get(`${api}bappeda/${id_bappeda}`);
+const getBappeda = (id_bappeda) =>
+  baseApi.get(`${api}bappeda/detail/${id_bappeda}`);
 
 const listKabupatenBappeda = () => baseApi.get(`${api}bappeda/kabupaten/list`);
 
-const addBappeda = (body) => baseApi.post(`${api}admin/bappeda/single`, body);
+const addBappeda = (body) => baseApi.post(`${api}bappeda`, body);
 
 const importBappeda = (body) =>
-  baseApi.post(`${api}admin/bappeda`, body, {
+  baseApi.post(`${api}bappeda/import`, body, {
     headers: {
       "Content-Type": "multipart/form-data",
     },
   });
 
 const editBappeda = (id_bappeda, body) =>
-  baseApi.put(`${api}admin/bappeda/${id_bappeda}`, body);
+  baseApi.put(`${api}bappeda/${id_bappeda}`, body);
 
 const deleteBappeda = (id_bappeda) =>
-  baseApi.delete(`${api}admin/bappeda/${id_bappeda}`);
+  baseApi.delete(`${api}bappeda/${id_bappeda}`);
 
 export {
   listBappeda,
