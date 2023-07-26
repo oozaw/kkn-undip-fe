981a6927c363963f9eecfdec2783c39ee313c030
oozaw <79121990+oozaw@users.noreply.github.com>
2023-07-26 16:58:43 +0700

    fix reviewer

--- src/service/reviewer.js
+++ src/service/reviewer.js
@@ -5,22 +5,22 @@ const api = `/`;
 const listReviewer = () => baseApi.get(`${api}reviewer`);
 
 const getReviewer = (id_reviewer) =>
-  baseApi.get(`${api}reviewer/${id_reviewer}`);
+  baseApi.get(`${api}reviewer/detail/${id_reviewer}`);
 
-const addReviewer = (body) => baseApi.post(`${api}admin/reviewer/single`, body);
+const addReviewer = (body) => baseApi.post(`${api}reviewer`, body);
 
 const importReviewer = (body) =>
-  baseApi.post(`${api}admin/reviewer`, body, {
+  baseApi.post(`${api}reviewer/import`, body, {
     headers: {
       "Content-Type": "multipart/form-data",
     },
   });
 
 const editReviewer = (id_reviewer, body) =>
-  baseApi.put(`${api}admin/reviewer/${id_reviewer}`, body);
+  baseApi.put(`${api}reviewer/${id_reviewer}`, body);
 
 const deleteReviewer = (id_reviewer) =>
-  baseApi.delete(`${api}admin/reviewer/${id_reviewer}`);
+  baseApi.delete(`${api}reviewer/${id_reviewer}`);
 
 export {
   listReviewer,
