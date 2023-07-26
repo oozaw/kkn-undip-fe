5ccc8f9fb4b32f95efad7f7ef0090ff086abb878
oozaw <79121990+oozaw@users.noreply.github.com>
2023-07-26 16:42:36 +0700

    fix korwil

--- src/service/korwil.js
+++ src/service/korwil.js
@@ -4,22 +4,22 @@ const api = `/`;
 
 const listKorwil = () => baseApi.get(`${api}korwil`);
 
-const getKorwil = (id_korwil) => baseApi.get(`${api}korwil/${id_korwil}`);
+const getKorwil = (id_korwil) =>
+  baseApi.get(`${api}korwil/detail/${id_korwil}`);
 
-const addKorwil = (body) => baseApi.post(`${api}admin/korwil/single`, body);
+const addKorwil = (body) => baseApi.post(`${api}korwil`, body);
 
 const importKorwil = (body) =>
-  baseApi.post(`${api}admin/korwil`, body, {
+  baseApi.post(`${api}korwil/import`, body, {
     headers: {
       "Content-Type": "multipart/form-data",
     },
   });
 
 const editKorwil = (id_korwil, body) =>
-  baseApi.put(`${api}admin/korwil/${id_korwil}`, body);
+  baseApi.put(`${api}korwil/${id_korwil}`, body);
 
-const deleteKorwil = (id_korwil) =>
-  baseApi.delete(`${api}admin/korwil/${id_korwil}`);
+const deleteKorwil = (id_korwil) => baseApi.delete(`${api}korwil/${id_korwil}`);
 
 export {
   listKorwil,
