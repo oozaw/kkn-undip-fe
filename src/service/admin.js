e84ae75ef2b9c8985efd92ac90395aa589b914f2
oozaw <79121990+oozaw@users.noreply.github.com>
2023-07-27 07:07:13 +0700

    fix superadmin

--- src/service/admin.js
+++ src/service/admin.js
@@ -4,11 +4,11 @@ const api = `/`;
 
 const listAdmin = () => baseApi.get(`${api}admin`);
 
-const getAdmin = (id_admin) => baseApi.get(`${api}admin/${id_admin}`);
+const getAdmin = (id_admin) => baseApi.get(`${api}admin/detail/${id_admin}`);
 
-const addAdmin = (body) => baseApi.post(`${api}superadmin/admin`, body);
+const addAdmin = (body) => baseApi.post(`${api}admin`, body);
 
 const editAdmin = (id_admin, body) =>
-  baseApi.put(`${api}superadmin/admin/${id_admin}`, body);
+  baseApi.put(`${api}admin/${id_admin}`, body);
 
 export { listAdmin, getAdmin, addAdmin, editAdmin };
