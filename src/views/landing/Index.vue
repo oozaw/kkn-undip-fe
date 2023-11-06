1849f852576bb261f8e23bb95396c6f677790c37
oozaw <79121990+oozaw@users.noreply.github.com>
2023-11-06 13:55:46 +0700

    (+) index and navbar landing page

---
+++ src/views/landing/Index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container top-0 position-sticky z-index-sticky">
+    <div class="row">
+      <div class="col-12">
+        <navbar
+          is-blur="blur  border-radius-lg my-3 py-2 start-0 end-0 mx-4 shadow"
+          btn-background="bg-gradient-success"
+          :dark-mode="true"
+        />
+      </div>
+    </div>
+  </div>
+  <main class="main-content mt-0"></main>
+</template>
+
+<script>
+import Navbar from "../partials/Navbar.vue";
+// import ArgonInput from "@/components/ArgonInput.vue";
+// import ArgonCheckbox from "@/components/ArgonCheckbox.vue";
+// import ArgonButton from "@/components/ArgonButton.vue";
+// import ArgonAlert from "@/components/ArgonAlert.vue";
+const body = document.getElementsByTagName("body")[0];
+import { mapMutations } from "vuex";
+
+export default {
+  name: "Landing",
+  components: {
+    Navbar,
+  },
+  created() {
+    this.$store.state.hideConfigButton = true;
+    this.toggleDefaultLayout();
+    body.classList.remove("bg-gray-100");
+  },
+  beforeUnmount() {
+    this.$store.state.hideConfigButton = false;
+    this.toggleDefaultLayout();
+    body.classList.add("bg-gray-100");
+  },
+  methods: {
+    ...mapMutations(["toggleDefaultLayout"]),
+  },
+};
+</script>
